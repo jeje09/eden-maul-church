90f4ead6d2007ea27a6baa3c1302c7b8519cf830
jeje09 <71625616+jeje09@users.noreply.github.com>
2022-01-27 02:23:00 +0900

    1st

---
+++ main.js
@@ -0,0 +1,11 @@
+let horizontalUnderline =document.getElementById("horizontal-underline");
+let horizontalMenus = document.querySelectorAll("nav:first-child a");
+
+horizontalMenus.forEach((menu) => menu.addEventListener("click", (e) => horizontalIndicator(e)));
+
+function horizontalIndicator(e){
+    horizontalUnderline.style.left = e.currentTarget.offsetLeft + "px";
+    horizontalUnderline.style.width = e.currentTarget.offsetWidth + "px";
+    horizontalUnderline.style.top = e.currentTarget.offsetTop + e.currentTarget.offsetHeight + "px";
+}
+
